cea0bc22adc5638ad8e2d97eb1558b7c13a76db5
AmolPrakash <44079434+AmolPrakash@users.noreply.github.com>
2019-12-05 15:37:03 +1100

    Delete Dialect reader .py

--- Dialect reader .py
+++
@@ -1,9 +0,0 @@
-import csv
-
-csv.register_dialect('x',delimiter=',',skipinitialspace=False)
-
-with open('/Users/amol/Library/Mobile Documents/com~apple~TextEdit/Documents/Untitled.csv','r') as a:
-    data=csv.reader(a,dialect='x')
-    for i in a:
-        print (i)
-
